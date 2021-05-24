192cdcc038037b6677143b5deb5e8f31cba3c2b0
Daphne <yeounga0107@naver.com>
2021-05-24 23:00:00 +0900

    JS : Arrow-up button

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ document.addEventListener('scroll', () => {
 
 // navbar의 메뉴 클릭 시 해당 섹션으로 스크롤 이동
 const navbarMenu = document.querySelector('.navbar__menu');
-navbarMenu.addEventListener('click',(event) => {
+navbarMenu.addEventListener('click', (event) => {
 
   const target = event.target;
   const link = target.dataset.link;
@@ -31,7 +31,7 @@ navbarMenu.addEventListener('click',(event) => {
 
 // Home의 Contact Me 버튼 클릭 시 #contact 섹션으로 스크롤 이동
 const homeContactBtn = document.querySelector('.home__contact');
-homeContactBtn.addEventListener('click',() => {
+homeContactBtn.addEventListener('click', () => {
   scrollToSection('#contact');
 });
 
@@ -44,8 +44,20 @@ document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
+// 스크롤 시 arrow-up 버튼 생성
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 5) {
+    arrowUp.classList.add('visible');
+  } else {
+    arrowUp.classList.remove('visible');
+  }
+});
 
-
+//  arrow-up 버튼 클릭 시 top으로 스크롤 이동
+arrowUp.addEventListener('click', () => {
+  scrollToSection('#home');
+});
 
 
 // 클릭 시 해당 섹션으로 스크롤 이동되는 함수 지정
