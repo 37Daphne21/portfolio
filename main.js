efe031b755f9e2df68fa210c5808ab383949dc1e
Daphne <yeounga0107@naver.com>
2021-05-24 21:26:06 +0900

    JS : Home contact button

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// navbar 투명, 스크롤 시 background-color 변경
+// navbar 스크롤 시 background-color 변경
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
@@ -15,7 +15,7 @@ document.addEventListener('scroll', () => {
   }
 });
 
-// navbar의 메뉴 클릭 시 해당 section으로 scroll 되도록
+// navbar의 메뉴 클릭 시 해당 섹션으로 스크롤 이동
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click',(event) => {
 
@@ -24,8 +24,19 @@ navbarMenu.addEventListener('click',(event) => {
   if (link == null) { // 원하는 data가 아닐 경우, 함수 리턴(더이상 밑에 있는 코드가 실행되지 않도록)
     return;
   }
-  console.log(event.target.dataset.link);
+  console.log(link);
 
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({behavior: "smooth"});
+  scrollToSection(link);
 });
+
+// Home의 Contact Me 버튼 클릭 시 #contact 섹션으로 스크롤 이동
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click',() => {
+  scrollToSection('#contact');
+});
+
+// 클릭 시 해당 섹션으로 스크롤 이동되는 함수 지정
+function scrollToSection(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
