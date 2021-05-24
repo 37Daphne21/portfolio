d98a1e7cbab3bc894c8aeefe544750b6d3ed0cd7
Daphne <yeounga0107@naver.com>
2021-05-24 20:27:26 +0900

    JS : Navbar menu click

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-  /*
+  /* 콘솔
   console.log(window.scrollY);
   console.log(`navbarHeight: ${navbarHeight}`);
   */
@@ -13,4 +13,19 @@ document.addEventListener('scroll', () => {
   } else {
     navbar.classList.remove('navbar--dark');
   }
+});
+
+// navbar의 메뉴 클릭 시 해당 section으로 scroll 되도록
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click',(event) => {
+
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) { // 원하는 data가 아닐 경우, 함수 리턴(더이상 밑에 있는 코드가 실행되지 않도록)
+    return;
+  }
+  console.log(event.target.dataset.link);
+
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({behavior: "smooth"});
 });
